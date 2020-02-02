6613b4ae9bd10c1274d471aeb38289e621cd80f9
MikeMarelo <stels1040533@gmail.com>
2020-02-03 01:44:17 +0300

    nice work

--- GameServerV1/Server/Room/RoomServer.cs
+++ GameServerV1/Server/Room/RoomServer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Data.SqlClient;
 using System.IO;
 using System.Threading;
 using System.Runtime.Serialization.Formatters.Binary;
